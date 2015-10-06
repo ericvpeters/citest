5e64e37d87fd3eca0af66fd17d6d5840ccc3dc87
Eric Peters <epeters@yfc.net>
2015-10-06 16:05:54 -0600

    added a test

---
+++ js/test/test.spec.js
@@ -0,0 +1,15 @@
+const { TestUtils } = React.addons;
+import expect from 'expect';
+
+function setup() {
+  
+}
+
+describe('components', () => {
+  jsdomReact();
+
+  describe('Header', () => {
+    it('should render correctly', () => {
+      expect(true).toEqual(true);
+    )};
+  });
